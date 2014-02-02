2d6a50744ddad0d782b5d934a454a167ab690de8
Blake Messer <rblakemesser@gmail.com>
2014-02-01 20:07:20 -0600

    Added hw1.iml and TheaterServer -- server file not finished

--- homework/hw1/src/TheaterServer.java
+++ homework/hw1/src/TheaterServer.java
@@ -9,13 +9,15 @@ public class TheaterServer {
     public OrderHandler orderHandler;
     public OrderReceiver orderReceiver;
     public UDPListen dgServer;
-
+    public static void main(String[] args) {
+    }
     public TheaterServer(int numSeats) {
         this.numSeats = numSeats;
         this.seatMap = new HashMap<Integer, String>();
         this.orderHandler = new OrderHandler(seatMap, numSeats);
         this.orderReceiver = new OrderReceiver(orderHandler);
         this.dgServer = new UDPListen(orderReceiver);
+
     }
 
 
