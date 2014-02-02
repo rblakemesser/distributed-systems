d2623b052f69c2722eca4d554065270da39cb7ee
Doug Gunter <gunter.doug@gmail.com>
2014-02-01 21:47:53 -0600

    TCP Server prototype in TCPListen class

---
+++ homework/hw1/theater/src/TCPListen.java
@@ -0,0 +1,36 @@
+/**
+ * Created by douglasgunter on 2/1/14.
+ */
+import java.io.*;
+import java.net.*;
+
+public class TCPListen {
+    int port;
+    int len = 1024;
+
+    public TCPListen() throws Exception{
+        this(2018);
+    }
+
+    public TCPListen(int port) throws Exception{
+        this.port = port;
+        ServerSocket tcpSocket = new ServerSocket(port);
+
+        while(true){
+            Socket connectionSocket = tcpSocket.accept();
+            byte[] buf = new byte[len];
+            /* Read the packet, find out what to do with it, and
+               pass it to the appropriate function
+             */
+            BufferedReader clientReader = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream clientReply = new DataOutputStream(connectionSocket.getOutputStream());
+            String clientCommand = clientReader.readLine();
+            /* Parse and handle the command
+               capture the reply from the server in response
+             */
+            String response = null;
+
+            clientReply.writeBytes(response);
+        }
+    }
+}
